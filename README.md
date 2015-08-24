a68c7c97ce7922ede0025da2b063e629cf327d48
Marcus Russi <marcusrussi@gmail.com>
2015-08-24 01:08:35 -0700

    Rewrote build system, added documentation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Hassler-frontend
-The frontend for the 2015 YO website
+The frontend for the 2015 YO website.
+
+To install dependencies, run
+```
+sudo npm install -g gulp
+npm install
+gulp
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,48 @@
 var gulp = require('gulp');
-var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
-var watchify = require('watchify');
 var reactify = require('reactify');
 var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
 
-gulp.task('browserify', function() {
-    var bundler = browserify({
-        entries: ['./app/maim.js'], // Only need initial file, browserify finds the deps
-        transform: [reactify], // We want to convert JSX to normal javascript
-        debug: true, // Gives us sourcemapping
-        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-    });
-    var watcher  = watchify(bundler);
 
-    return watcher
-    .on('update', function () { // When any files update
-        var updateStart = Date.now();
-        console.log('Updating!');
-        watcher.bundle() // Create new bundle that uses the cache for high performance
-        .pipe(source('main.js'))
-    // This is where you add uglifying etc.
-        .pipe(gulp.dest('./build/'));
-        console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
-    .bundle() // Create the initial bundle when starting the task
+gulp.task('scripts', function () {
+
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './app/main.js',
+    debug: true,
+    transform: [reactify]
+  });
+
+  return b.bundle()
     .pipe(source('main.js'))
-    .pipe(gulp.dest('./build/'));
+    .pipe(buffer())
+    // .pipe(sourcemaps.init({loadMaps: true}))
+      // Add transformation tasks to the pipeline here.
+    // .pipe(uglify())
+    .on('error', gutil.log)
+    // .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./build'))
+    .pipe(notify('JS compiled'));
 });
 
-// I added this so that you see how to run two watch tasks
 gulp.task('css', function () {
-    gulp.watch('styles/**/*.css', function () {
-        return gulp.src('styles/**/*.css')
+    gulp.src('css/**/*.css')
         .pipe(concat('main.css'))
-        .pipe(gulp.dest('build/'));
-    });
+        .pipe(gulp.dest('build'))
+        .pipe(notify('CSS concatenated'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('css/**/*.css', ['css']);
+  gulp.watch('app/**/*.js', ['scripts']);
+})
 
 // Just running the two tasks
-gulp.task('default', ['browserify', 'css']);
+gulp.task('default', ['scripts', 'css', 'watch']);
+
